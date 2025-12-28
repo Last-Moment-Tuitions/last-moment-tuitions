87d1dfa546beb5f0012c1c0c8299f669668225b3
Monu Mahto <monumahto7864@gmail.com>
2025-12-29 01:18:45 +0530

    redis setup

--- backend/src/redis/redis.module.ts
+++ backend/src/redis/redis.module.ts
@@ -13,6 +13,8 @@ import Redis from 'ioredis';
                 return new Redis({
                     host: configService.get<string>('REDIS_HOST'),
                     port: configService.get<number>('REDIS_PORT'),
+                    username: configService.get<string>('REDIS_USERNAME'),
+                    password: configService.get<string>('REDIS_PASSWORD'),
                 });
             },
             inject: [ConfigService],
