fc51cd2b5e8b09e346ca0e5ddcbdb38825d32be0
Monu Mahto <monumahto7864@gmail.com>
2026-02-15 23:03:55 +0530

    ENV fix

--- frontend/src/services/testimonialService.js
+++ frontend/src/services/testimonialService.js
@@ -1,4 +1,4 @@
-const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_API;
 const baseUrl = `${BACKEND_URL}/api`;
 
 const getAuthHeaders = () => {
