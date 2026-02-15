08ed0cec080bfc2091d666dfb25cea5961acb7b5
STRANGER-Github <yashg1973@gmail.com>
2026-02-15 13:59:21 +0530

    Finalized Testimonial

--- backend/src/modules/testimonials/testimonials.module.ts
+++ backend/src/modules/testimonials/testimonials.module.ts
@@ -4,12 +4,15 @@ import { TestimonialsController } from './testimonials.controller';
 import { TestimonialsService } from './testimonials.service';
 import { Testimonial, TestimonialSchema } from './schemas/testimonial.schema';
 import { AuthModule } from '../../auth/auth.module';
+import { UsersModule } from '../../users/users.module';
+
 
 @Module({
     imports: [
         MongooseModule.forFeature([
             { name: Testimonial.name, schema: TestimonialSchema }
         ]),
+        UsersModule,
         AuthModule,
     ],
     controllers: [TestimonialsController],
